993f786fc2908ce4b5166ee2f0fd58462d2cb4d8
Ruben Izquierdo <r.izquierdobevia@vu.nl>
2015-02-10 13:49:37 +0100

    Updated

--- my_elasticsearch.py
+++ my_elasticsearch.py
@@ -14,6 +14,7 @@ import os
 import sys
 import signal
 import time
+import logging
 from subprocess import Popen, PIPE
 from libs import elasticsearch
 
@@ -21,9 +22,21 @@ SUBPATH_TO_ES=os.path.join('libs','elasticsearch','bin','elasticsearch')
 
 class Celastic_search:
     def __init__(self):
+        self.create_logger()
         self.here = os.path.dirname(os.path.realpath(__file__))
         self.es = elasticsearch.Elasticsearch()
         self.pid_process_elasticsearch = None
+        if not self.is_up():
+            self.start()
+        
+    def create_logger(self):
+        self.logger = logging.getLogger(__file__)
+    
+        handler = logging.StreamHandler(sys.stderr)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+        self.logger.setLevel(logging.INFO)
         
     def is_up(self):
         return self.es.ping()
@@ -33,19 +46,56 @@ class Celastic_search:
             whole_path_es = os.path.join(self.here,SUBPATH_TO_ES)
             es_proc = Popen(whole_path_es, stdout=PIPE, stderr=PIPE,stdin=PIPE)
             self.pid_process_elasticsearch = es_proc.pid
-            print>>sys.stderr,'Running ES with pid', self.pid_process_elasticsearch
+            self.logger.info('Starting Elastic search with pid %s' % self.pid_process_elasticsearch)
             while not self.is_up():
                 time.sleep(1)
         else:
-            print>>sys.stderr,'Elastic search is already running'
+            self.logger.info('Elastic search is already running')
             
     def stop(self):
         if self.pid_process_elasticsearch is not None:
             os.kill(self.pid_process_elasticsearch, signal.SIGKILL)
-
-            print>>sys.stderr,'Kill process', self.pid_process_elasticsearch
+            self.logger.info('Stopping Elastic search with pid %s' % self.pid_process_elasticsearch)
         else:
-            print>>sys.stderr,'This object did not launch ElasticSearch and can not stop it'
+            self.logger.info('This client did not launch ElasticSearch and can not stop it')
+            
+            
+    def index_exists(self,idx_name):
+        '''
+        Checks if the given index exists or not
+        '''
+        return self.es.indices.exists(idx_name)
+    
+    def create_index(self,idx_name):
+        '''
+        Create the given index
+        '''
+        self.es.indices.create(index=idx_name)
+        
+    def search_exists(self, my_index, my_doc_type, dsl_query):
+        exists = False
+        try:
+            res = self.es.search_exists(index = my_index, doc_type=my_doc_type, body = dsl_query)
+            exists = res['exists']
+        except:
+            exists = False
+        return exists
+    
+    def search(self, my_index, my_doc_type, dsl_query):
+        res = None
+        try:
+            res = self.es.search(index = my_index, doc_type=my_doc_type, body = dsl_query)
+        except:
+            res = None
+        return res
+    
+    def index_document(self, my_index, my_doc_type, dsl_doc):
+        res = None
+        try:
+            res = self.es.index(index=my_index, doc_type = my_doc_type, body = dsl_doc)
+        except:
+            res = None
+        return res
             
     
                  
